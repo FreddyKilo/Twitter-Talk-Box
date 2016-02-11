e2af0a8ca25cd2d5c43d5c4b1be3391d84a9e584
FreddyKilo <freddy.kilo@gmail.com>
2016-02-10 20:41:21 -0700

    add main()

--- UserPostListner.py
+++ UserPostListner.py
@@ -10,12 +10,15 @@ con_key, con_secret = read_token_file(CONSUMER_KEYS)
 auth = OAuth(consumer_key=con_key, consumer_secret=con_secret, token=oauth_token, token_secret=oauth_secret)
 stream = TwitterStream(auth=auth, domain='userstream.twitter.com')
 
-for tweet in stream.user():
-    text = tweet.get('text')
-    if text is not None:
-        if USERNAME in text:
-            text = text[len(USERNAME) + 1:]
-        user = tweet.get('user').get('name')
-        say = user + " said " + text
-        print(say)
-        os.system("say " + say)
+def main():
+    for tweet in stream.user():
+        text = tweet.get('text')
+        if text is not None:
+            if USERNAME in text:
+                text = text[len(USERNAME) + 1:]
+            user = tweet.get('user').get('name')
+            say = user + " said " + text
+            print(say)
+            os.system("say " + say)
+
+main()
